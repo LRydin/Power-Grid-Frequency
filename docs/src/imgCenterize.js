da6b6bbeb1ad78610a82f141a24c612647d26586
LRydin <leonardo.rydin@gmail.com>
2021-03-31 10:54:13 +0200

    removing unused docs

--- docs/src/imgCenterize.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", function () {
-  const imgs = Array.from(document.getElementsByTagName("img"));
-  imgs.forEach((img) => {
-    img.parentElement.classList.add("imgParent");
-  });
-});
